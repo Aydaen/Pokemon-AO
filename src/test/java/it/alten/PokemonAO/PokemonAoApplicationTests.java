ca15ec37964e7ce38adcfb344160012228bc1661
Giordano <giordano.lombardo01@gmail.com>
2024-04-16 10:18:04 +0200

    feat: create project structure

---
+++ src/test/java/it/alten/PokemonAO/PokemonAoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.alten.PokemonAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
