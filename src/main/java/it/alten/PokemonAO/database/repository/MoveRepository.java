693faed7eedc3362d41f5f8c9b2760ab94116373
Pablo NARDECCHIA <pablo.nardecchia@alten.it>
2024-04-16 14:55:14 +0200

    Revert "create Entity and Repository"
    This reverts commit c4d72584a633b43fdf67d755ccc0eb7f833c90f4.

--- src/main/java/it/alten/PokemonAO/database/repository/MoveRepository.java
+++
@@ -1,9 +0,0 @@
-package it.alten.PokemonAO.database.repository;
-
-import it.alten.PokemonAO.database.entity.Move;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MoveRepository extends JpaRepository<Move,Integer> {
-}
--- src/main/java/it/alten/PokemonAO/database/repository/TypeRepository.java
+++
@@ -1,9 +0,0 @@
-package it.alten.PokemonAO.database.repository;
-
-import it.alten.PokemonAO.database.entity.Type;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TypeRepository extends JpaRepository<Type,Integer>{
-}
