1d8fb27ea5b5a75fb3337775b818d1a168e58741
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2024-04-16 17:41:42 +0200

    fix: change files name

---
+++ src/main/java/it/alten/pokemonao/database/entity/MoveEntity.java
@@ -0,0 +1,33 @@
+package it.alten.pokemonao.database.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "move", schema = "pokemon_schema")
+public class MoveEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Integer id;
+
+    @Column(name = "name", length = Integer.MAX_VALUE, nullable = false, updatable = false)
+    private String name;
+
+    @Column(name = "power", nullable = false)
+    private Integer power;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "type_id")
+    private TypeEntity typeEntity;
+
+    @ManyToMany(mappedBy = "moveEntities")
+    private Set<PokemonEntity> pokemonEntity = new HashSet<>();
+
+
+}
---
+++ src/main/java/it/alten/pokemonao/database/entity/TypeEntity.java
@@ -0,0 +1,28 @@
+package it.alten.pokemonao.database.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "type", schema = "pokemon_schema")
+public class TypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Integer id;
+
+    @Column(name = "name", length = Integer.MAX_VALUE, nullable = false, updatable = false)
+    private String name;
+
+    @Column(name = "icon_url", length = Integer.MAX_VALUE)
+    private String iconUrl;
+
+    @OneToMany(mappedBy = "typeEntity")
+    private Set<MoveEntity> moveEntities = new HashSet<>();
+
+    @OneToMany(mappedBy = "typeEntity")
+    private Set<PokemonEntity> pokemonEntities = new HashSet<>();
+
+}
