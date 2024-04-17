a762fc91f5ba43b35659b3b639e3b2044717fc6c
Pablo NARDECCHIA <pablo.nardecchia@alten.it>
2024-04-17 17:23:42 +0200

    feat: add post request

--- src/main/java/it/alten/pokemonao/controllers/TypeController.java
+++ src/main/java/it/alten/pokemonao/controllers/TypeController.java
@@ -6,10 +6,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +23,11 @@ public class TypeController {
         }catch (EntityNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody TypeDTO request) {
+        typeService.save(request);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
