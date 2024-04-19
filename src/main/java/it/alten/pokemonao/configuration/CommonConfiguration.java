5b12896e862e2e07b84b1c7469f463293cc68d5e
Giordano <giordano.lombardo01@gmail.com>
2024-04-19 11:48:41 +0200

    feat: add corsConfigurer to consent front-back communication

--- src/main/java/it/alten/pokemonao/configuration/CommonConfiguration.java
+++ src/main/java/it/alten/pokemonao/configuration/CommonConfiguration.java
@@ -3,11 +3,27 @@ package it.alten.pokemonao.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CommonConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
