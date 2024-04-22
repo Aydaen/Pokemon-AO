74b0260182d6cfe86616fcfd48477e57859bd6d6
lombardo <giordano.lombardo01@gmail.com>
2024-04-22 09:40:12 +0200

    feat: add findByName method

--- src/main/java/it/alten/pokemonao/database/repository/TypeRepository.java
+++ src/main/java/it/alten/pokemonao/database/repository/TypeRepository.java
@@ -1,9 +1,13 @@
 package it.alten.pokemonao.database.repository;
 
 import it.alten.pokemonao.database.entity.TypeEntity;
+import it.alten.pokemonao.dtos.TypeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TypeRepository extends JpaRepository<TypeEntity,Integer> {
+    Optional<TypeEntity> findByName(String name);
 }
